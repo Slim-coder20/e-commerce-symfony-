6a1e549db8e794e8ee11734d88b67ea3548ddcfb
slim <slimabida21@gmail.com>
2025-05-14 09:29:49 +0200

    Mise a jour dans l'entité Product avec l'ajout du chamm image et le rajouter dans ProductFormType et el template form.html.twig

--- src/Form/ProductForm.php
+++ src/Form/ProductForm.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class ProductForm extends AbstractType
 {
@@ -17,6 +18,12 @@ class ProductForm extends AbstractType
             ->add('name')
             ->add('description')
             ->add('price')
+            ->add('image', FileType::class,[
+                'label' => ' image du produit',
+                'format' => 'jpg, png, jpeg',
+                'mapped' => false,
+                'required' => false,
+                ])
             ->add('subCategories', EntityType::class, [
                 'class' => Subcategory::class,
                 'choice_label' => 'name',
