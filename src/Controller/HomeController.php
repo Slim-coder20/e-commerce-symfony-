35a3ff27bdaf9b924bc77af90745067417310ce2
slim <slimabida21@gmail.com>
2025-05-16 19:32:09 +0200

    mise en place de pagination dans la méthode index dans le HomeController test OK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,24 +7,35 @@ use App\Repository\CategoryRepository;
 use App\Entity\Product;
 use App\Repository\SubcategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository ): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository, Request $request, PaginatorInterface $paginatorInterface ): Response
     {
+        $data = $productRepository->findBy([],['id' => 'DESC']);
+        $product = $paginatorInterface->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            6
+        );
+        
+        
+        
+        
+        
         return $this->render('home/home.html.twig', [
-            'products' => $productRepository->findBy([],['id' => 'DESC']),
-            'categories' => $categoryRepository->findAll(),
+            'products' => $product
+,            'categories' => $categoryRepository->findAll(),
         ]);
     }
     
     // cette route va nous sevire a afficher les détails d'un produit selectionné depuis la page d'accueil // 
-    
-    
     #[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
     public function showProduct (Product $product, ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
@@ -38,6 +49,7 @@ final class HomeController extends AbstractController
         ]);
 
     }
+    
     // cette route va nous permettre de filtrer les produits par catégories // 
     #[Route('/home/product/subcategory/{id}/filter}', name: 'app_home_product_filter', methods: ['GET'])]
     public function filter ($id ,SubcategoryRepository $subcategoryRepository,CategoryRepository $categoryRepository): Response
