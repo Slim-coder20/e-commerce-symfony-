33e45b30455e9365ad379418ce204d3dc4bb3f1b
slim <slimabida21@gmail.com>
2025-05-16 11:50:19 +0200

    création d'une nouvelle route pour afficher les produit selectionné depuis l'accueil et création du template show.html.twig avec l'afichage des derniers produit ajouté via le productRepository

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): Response
     {
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $productRepository->findAll(),
         ]);
     }
+    
+    // cette route va nous sevire a afficher les détails d'un produit selectionné depuis la page d'accueil // 
+    
+    
+    #[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
+    public function showProduct (Product $product, ProductRepository $productRepository): Response
+    {
+        $lastProducts = $productRepository->findBy([], ['id' => 'DESC'], 3);
+        
+        
+        return $this->render('home/show.html.twig', [
+            'product' => $product,
+            'products' => $lastProducts,
+        ]);
+    }
+
+
+
+
+
+
+
+
 }
