7b820bb06fef5c73e15314d3726ce28ba73d01f4
slim <slimabida21@gmail.com>
2025-05-16 17:00:38 +0200

    mise en place du filtre pour afficher les catégories et sous catégories dans le menu de navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
 use App\Entity\Product;
+use App\Repository\SubcategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,10 +14,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository ): Response
     {
         return $this->render('home/home.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $productRepository->findBy([],['id' => 'DESC']),
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
     
@@ -23,7 +26,7 @@ final class HomeController extends AbstractController
     
     
     #[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
-    public function showProduct (Product $product, ProductRepository $productRepository): Response
+    public function showProduct (Product $product, ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
         $lastProducts = $productRepository->findBy([], ['id' => 'DESC'], 3);
         
@@ -31,6 +34,25 @@ final class HomeController extends AbstractController
         return $this->render('home/show.html.twig', [
             'product' => $product,
             'products' => $lastProducts,
+            'categories' => $categoryRepository->findAll(),
+        ]);
+
+    }
+    // cette route va nous permettre de filtrer les produits par catégories // 
+    #[Route('/home/product/subcategory/{id}/filter}', name: 'app_home_product_filter', methods: ['GET'])]
+    public function filter ($id ,SubcategoryRepository $subcategoryRepository,CategoryRepository $categoryRepository): Response
+    {
+        
+        $products = $subcategoryRepository->find($id)->getProducts();
+        $subcategory = $subcategoryRepository->find($id);
+        
+   
+            
+        return $this->render('home/filter.html.twig', [
+        'products' => $products,
+        'subcategory' => $subcategory,
+        'categories' => $categoryRepository->findAll(),
+            
         ]);
     }
 
