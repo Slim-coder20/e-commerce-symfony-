654d99437a70933cc8bcc68e59c97874ebc58581
slim <slimabida21@gmail.com>
2025-05-18 00:02:41 +0200

    Mise a jour de la table Order ajout du champ createdAt migration vers BDD OK

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -29,6 +29,9 @@ class Order
     #[ORM\ManyToOne(inversedBy: 'orders')]
     private ?City $city = null;
 
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -90,6 +93,18 @@ class Order
     public function setCity(?City $city): static
     {
         $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
